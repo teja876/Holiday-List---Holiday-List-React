46be18ff7402444aa92a8db47cb81810a25f6bef
HSM256 <harsha.katakam123@gmail.com>
2020-10-19 18:05:03 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    const places = ["Goa", "Darjeeling", "Lonavala"];
+    return (
+      <>
+        <ol>
+          {places.map((m) => (
+            <li key={m}>{m}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
